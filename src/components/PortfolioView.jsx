27f775da1e2edad152639ec64c57203af64536b0
James Westhead <james_westhead@outlook.com>
2026-01-19 00:22:44 +0000

    had to change the location

--- src/components/PortfolioView.jsx
+++ src/components/PortfolioView.jsx
@@ -43,7 +43,7 @@ export default function PortfolioView() {
                     <span className="absolute px-16 font-medium text-heading -translate-x-1/2 bg-neutral-950 left-1/2"><p className="text-xl">CV/Resume</p></span>
                 </div>
                 <a
-                    href="../../CV_James_Westhead.pdf"
+                    href="/assets/CV_James_Westhead.pdf"
                     download
                     aria-label="Download James Westhead CV"
                     className="inline-flex items-center px-6 py-3 bg-amber-100 text-black font-semibold rounded-lg shadow hover:bg-amber-200 transition-colors duration-200"
