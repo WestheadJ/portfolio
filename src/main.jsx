a7d5928644b4981eb589d959c48a42bf18c81def
James Westhead <james_westhead@outlook.com>
2026-01-23 13:14:30 +0000

    renames

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,20 @@ import App from "./App";
 import "./index.css";
 import Nav from "./components/Nav";
 import { BrowserRouter, Route, Routes, } from "react-router";
-import Sales_To_Stocks from "@components/projects/Sales-To-Stocks";
+import Sales_To_Stocks from "@components/projects/SalesToStocks";
 import HashScrollHandler from "@handlers/hashScrollHandler"
-import AppleMusicToSpotify from "./components/projects/apple-music-to-spotify";
+import AppleMusicToSpotify from "@components/projects/AppleMusicToSpotify";
+import ExceptionTool from "@components/projects/ExceptionTool";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <Nav />
 
+
         <BrowserRouter> <HashScrollHandler /><Routes><Route path="/" element={<App />} />
             <Route path="/projects/sales-to-stocks" element={<Sales_To_Stocks />} />
             <Route path="/projects/apple-music-to-spotify" element={<AppleMusicToSpotify />} />
+            <Route path="/projects/exception-tool" element={<ExceptionTool />} />
         </Routes></BrowserRouter>
     </React.StrictMode>
 );
